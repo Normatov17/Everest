cc23d01d1ad67a7d788e482b0f0a6e819c030626
DavlatbekNormatov <davlatbeknormatov17@gmail.com>
2023-06-23 21:40:11 +0500

    swipper

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next1",
+    prevEl: ".swiper-button-prev1",
+  },
+});
+
+if (swiper === 0) {
+}
